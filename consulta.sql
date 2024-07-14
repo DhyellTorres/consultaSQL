aaf6674ef8d953e36a1778a359589229bd4a5bd4
Adryel Torres <45578170+DhyellTorres@users.noreply.github.com>
2024-07-14 00:22:51 -0300

    Create consulta.sql

---
+++ consulta.sql
@@ -0,0 +1,8 @@
+-- Consulta para obter os funcionários com as três maiores médias salariais dos últimos 3 meses
+SELECT e.id, e.name, AVG(s.value) AS average_salary
+FROM employee e
+JOIN salary s ON e.id = s.employee_id
+WHERE s.payment_date >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
+GROUP BY e.id, e.name
+ORDER BY average_salary DESC
+LIMIT 3;
